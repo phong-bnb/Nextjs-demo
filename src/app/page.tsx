152cda5656ce12a5cbc9fec65ff0ff758419426a
Gia Phong <ngphong.7800@gmail.com>
2024-06-23 09:41:59 +0700

    nextjs-demo

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <WalletMultiButton/>
+    </div>
+  )
+}
+
+export default page
